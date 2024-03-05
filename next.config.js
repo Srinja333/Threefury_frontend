fd99ee354c6c3af9fa5ab921d8fa48a81d86a0c9
Srinjay Kapri <srinjaayan1262000@gmail.com>
2024-03-05 14:25:13 +0530

    comm

--- next.config.js
+++ next.config.js
@@ -1,24 +1,9 @@
 /** @type {import('next'). NextConfig} */
 const webpack = require("webpack");
 const nextConfig = {
-//reactStrictMode: true,
-distDir: 'dist',
-output: 'export',
 images: {
   loader: 'custom',
   loaderFile: './my-loader.js',
-},
-//output: 'export',
-webpack: (config, {buildId, dev, isServer, defaultLoaders, webpack }) =>
-{
-config.plugins.push(
-new webpack. ProvidePlugin({
-$: "jquery",
-jQuery: "jquery",
-"window.jQuery": "jquery",
-})
-);
-return config;
 },
 };
 module.exports = nextConfig;
